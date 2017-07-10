05330b34e350d6919f85ffcd965f73c628acf55a
Roland Kammerer <roland.kammerer@linbit.com>
2017-07-10 13:01:28 +0200

    build: switch docker baseimage alpine -> ubuntu
    Unfortunately inkscape in alpine has a bug, so for now switch to ubuntu
    even if it pulls in have of the world as a dependency.

--- build_docker_image.sh
+++ build_docker_image.sh
@@ -1,23 +1,35 @@
 #!/bin/bash
 
 function generate_dockerfile {
+# cat <<EOF > Dockerfile
+# FROM alpine:latest
+# MAINTAINER Roland Kammerer <roland.kammerer@linbit.com>
+#
+# RUN addgroup -g $(id -g) makedoc
+# RUN adduser -u $(id -u) -G makedoc -D makedoc
+#
+# RUN apk add --no-cache ruby ruby-irb ruby-rdoc make sed inkscape
+# RUN gem install --pre asciidoctor-pdf
+#
+# USER makedoc
+# EOF
 cat <<EOF > Dockerfile
-FROM alpine:latest
+FROM ubuntu:xenial
 MAINTAINER Roland Kammerer <roland.kammerer@linbit.com>
 
-RUN addgroup -g $(id -g) makedoc
-RUN adduser -u $(id -u) -G makedoc -D makedoc
+RUN groupadd --gid $(id -g) makedoc
+RUN useradd -u $(id -u) -g $(id -g) makedoc
 
-RUN apk add --no-cache ruby ruby-irb ruby-rdoc make sed inkscape
+RUN apt update -y && apt install -y make inkscape ruby
 RUN gem install --pre asciidoctor-pdf
 
 USER makedoc
 EOF
 }
 
-if [ ! -e Dockerfile ]; then
+if [ -z "$(docker images --format={{.Repository}}:{{.Tag}} | egrep 'linbit-documentation:latest')" ]; then
 	generate_dockerfile
 	docker build -t linbit-documentation .
+else
+	echo "linbit-documentation:latest base image already exists"
 fi
-
-# docker run -it --rm -v $PWD:/home/makedoc/asciidoctor asciidoctor /bin/sh -c "cd /home/makedoc/asciidoctor && ./make_documents.sh $*"
