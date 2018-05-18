f5c59ff46ef7fe52eb510ec375183201657bd56e
Roland Kammerer <roland.kammerer@linbit.com>
2018-05-18 11:10:03 +0200

    add ja sub targets, but do not build by default

--- UG-top.mk
+++ UG-top.mk
@@ -14,3 +14,15 @@ en-html-finalize:
 
 en-pdf-finalize:
 	make -C en pdf-finalize
+
+ja-html:
+	make -C ja html
+
+ja-pdf:
+	make -C ja pdf
+
+ja-html-finalize:
+	make -C ja html-finalize
+
+ja-pdf-finalize:
+	make -C ja pdf-finalize
