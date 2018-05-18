86f82f87abf28606883891e682395b79bfb1c61c
Roland Kammerer <roland.kammerer@linbit.com>
2018-05-18 11:54:49 +0200

    top Makefile: add clean

--- UG-top.mk
+++ UG-top.mk
@@ -8,11 +8,14 @@ html: $(lang)/html
 html-finalize: $(lang)/html-finalize
 pdf: $(lang)/pdf
 pdf-finalize: $(lang)/pdf-finalize
+clean: $(lang)/clean
 
 known-targets := $(foreach l,$(languages),$(addprefix $(l)/,$(formats)))
 known-targets += $(addsuffix -finalize,$(known-targets))
+# clean targets
+known-targets += $(foreach l,$(languages),$(addprefix $(l)/,clean))
 
 $(known-targets):
 	make -C $(@D) $(@F)
 
-.PHONY: $(known-targets) html html-finalize pdf pdf-finalize
+.PHONY: $(known-targets) html html-finalize pdf pdf-finalize clean
