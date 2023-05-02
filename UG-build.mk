1589971b5fa4bacee02d982c130ff70a07767591
Christoph Böhmwalder <christoph.boehmwalder@linbit.com>
2023-05-02 15:27:53 +0200

    add guide: Understanding LINSTOR Gateway

--- UG-build.mk
+++ UG-build.mk
@@ -10,6 +10,7 @@
 IN_UG=drbd-users-guide.adoc
 IN_LS=linstor-users-guide.adoc
 IN_VSAN=vsan-users-guide.adoc
+IN_UNDERSTANDING_GW=linstorgw-users-guide.adoc
 OUTDIR=output
 OUTDIRPDF=$(OUTDIR)-pdf
 OUTDIRHTML=$(OUTDIR)-html
@@ -24,6 +25,7 @@ STYLESDIR=../../styles
 OUTHTML_UG=$(addsuffix .html,$(basename $(IN_UG)))
 OUTHTML_LS=$(addsuffix .html,$(basename $(IN_LS)))
 OUTHTML_VSAN=$(addsuffix .html,$(basename $(IN_VSAN)))
+OUTHTML_UNDERSTANDING_GW=$(addsuffix .html,$(basename $(IN_UNDERSTANDING_GW)))
 # The -WEBDEVS variables below should result in file names in the form:
 # xxxxxxx-users-guide-without-css.html, e.g.,
 # vsan-users-guide-without-css.html. This is the HTML file that will be
@@ -33,9 +35,11 @@ OUTHTML_VSAN=$(addsuffix .html,$(basename $(IN_VSAN)))
 OUTHTML_UG_WEBDEVS=$(addsuffix -without-css.html,$(basename $(IN_UG)))
 OUTHTML_LS_WEBDEVS=$(addsuffix -without-css.html,$(basename $(IN_LS)))
 OUTHTML_VSAN_WEBDEVS=$(addsuffix -without-css.html,$(basename $(IN_VSAN)))
+OUTHTML_UNDERSTANDING_GW_WEBDEVS=$(addsuffix -without-css.html,$(basename $(IN_UNDERSTANDING_GW)))
 OUTPDF_UG=$(addsuffix .pdf,$(basename $(IN_UG)))
 OUTPDF_LS=$(addsuffix .pdf,$(basename $(IN_LS)))
 OUTPDF_VSAN=$(addsuffix .pdf,$(basename $(IN_VSAN)))
+OUTPDF_UNDERSTANDING_GW=$(addsuffix .pdf,$(basename $(IN_UNDERSTANDING_GW)))
 
 # for HTML
 SVGS=$(addprefix ../../, $(SVGSUSED))
@@ -61,6 +65,7 @@ $(OUTDIRHTMLIMAGES)/%.png: $(IMGDIR)/%.png
 
 $(OUTDIRHTML)/%.adoc: %.adoc
 #	sed 's/\(^image::.*\)\.svg\(.*\)/\1.png\2/g' $< > $@
+	echo mkdir -p $@/$(shell dirname $<)
 	cp $< $@
 
 $(OUTDIRHTML)/$(OUTHTML_UG): $(OUTDIRHTMLIMAGES) $(SRC) $(OUTSVGSSVGS) $(OUTPNGSPNGS) $(OUTADOCS)
@@ -80,7 +85,14 @@ $(OUTDIRHTML)/$(OUTHTML_VSAN): $(OUTDIRHTMLIMAGES) $(SRC) $(OUTSVGSSVGS) $(OUTPN
 		echo "VSAN guide not found"; \
 	fi
 
-html: $(OUTDIRHTML)/$(OUTHTML_UG) $(OUTDIRHTML)/$(OUTHTML_LS) $(OUTDIRHTML)/$(OUTHTML_VSAN)
+$(OUTDIRHTML)/$(OUTHTML_UNDERSTANDING_GW): $(OUTDIRHTMLIMAGES) $(SRC) $(OUTSVGSSVGS) $(OUTPNGSPNGS) $(OUTADOCS)
+	if test -f $(OUTDIRHTML)/$(IN_UNDERSTANDING_GW); then \
+		asciidoctor $(ASCIIDOCTOR_ADD_OPTIONS) -n -d book -a toc=left -a linkcss -a sectanchors=yes $(OPTS) -o ./$(OUTDIRHTML)/$(OUTHTML_UNDERSTANDING_GW) $(OUTDIRHTML)/$(IN_UNDERSTANDING_GW); \
+	else \
+		echo "Understanding LINSTOR Gateway guide not found"; \
+	fi
+
+html: $(OUTDIRHTML)/$(OUTHTML_UG) $(OUTDIRHTML)/$(OUTHTML_LS) $(OUTDIRHTML)/$(OUTHTML_VSAN) $(OUTDIRHTML)/$(OUTHTML_UNDERSTANDING_GW)
 	@echo "Generated web pages in $$(pwd)/$(OUTDIRHTML)"
 	@echo "Execute 'make html-finalize' to prepare upload"
 
@@ -101,6 +113,12 @@ html-finalize: html
 		td=$$(mktemp -d) && \
 		cp $(OUTDIRHTML)/$(OUTHTML_VSAN_WEBDEVS) "$$td"/$(OUTHTML_VSAN_WEBDEVS) && cp -r $(OUTDIRHTMLIMAGES) $$td && \
 		(cd $$td && zip vsan.zip $(OUTHTML_VSAN_WEBDEVS) images/*.*) && mv $$td/vsan.zip $(OUTDIRHTMLFINAL) && rm -rf "$$td"; \
+	fi
+	if test -f $(OUTDIRHTML)/$(OUTHTML_UNDERSTANDING_GW); then \
+		mv $(OUTDIRHTML)/$(OUTHTML_UNDERSTANDING_GW) $(OUTDIRHTML)/$(OUTHTML_UNDERSTANDING_GW_WEBDEVS) && \
+		td=$$(mktemp -d) && \
+		cp $(OUTDIRHTML)/$(OUTHTML_UNDERSTANDING_GW_WEBDEVS) "$$td"/$(OUTHTML_UNDERSTANDING_GW_WEBDEVS) && cp -r $(OUTDIRHTMLIMAGES) $$td && \
+		(cd $$td && zip understanding-gw.zip $(OUTHTML_UNDERSTANDING_GW_WEBDEVS) images/*.*) && mv $$td/understanding-gw.zip $(OUTDIRHTMLFINAL) && rm -rf "$$td"; \
 	fi
 
 # PDF
@@ -128,8 +146,15 @@ $(OUTDIRPDF)/$(OUTPDF_VSAN): $(SRC) $(SVGSUSED)
 		INTERN=""; fi && \
 		if test -f $(IN_VSAN); then asciidoctor-pdf $(ASCIIDOCTOR_ADD_OPTIONS) -d book $$INTERN $(OPTS) -o $@ $(IN_VSAN); fi
 
-pdf: ./images $(OUTDIRPDF)/$(OUTPDF_UG) $(OUTDIRPDF)/$(OUTPDF_LS) $(OUTDIRPDF)/$(OUTPDF_VSAN)
-	@echo "Generated $$(pwd)/$(OUTDIRPDF)/{$(OUTPDF_UG),$(OUTPDF_LS),$(OUTPDF_VSAN)}"
+$(OUTDIRPDF)/$(OUTPDF_UNDERSTANDING_GW): $(SRC) $(SVGSUSED)
+	if [ -d $(FONTDIR) ] && [ "$(lang)" != "cn" ] && \
+		! echo "$(OPTS)" | grep -qFw 'de-brand'; then \
+		INTERN="--theme $(THEMESDIR)/pdf-$(lang)-theme.yml -a pdf-fontsdir=$(FONTDIR)"; else \
+		INTERN=""; fi && \
+		if test -f $(IN_UNDERSTANDING_GW); then asciidoctor-pdf $(ASCIIDOCTOR_ADD_OPTIONS) -d book $$INTERN $(OPTS) -o $@ $(IN_UNDERSTANDING_GW); fi
+
+pdf: ./images $(OUTDIRPDF)/$(OUTPDF_UG) $(OUTDIRPDF)/$(OUTPDF_LS) $(OUTDIRPDF)/$(OUTPDF_VSAN) $(OUTDIRPDF)/$(OUTPDF_UNDERSTANDING_GW)
+	@echo "Generated $$(pwd)/$(OUTDIRPDF)/{$(OUTPDF_UG),$(OUTPDF_LS),$(OUTPDF_VSAN),$(OUTPDF_UNDERSTANDING_GW)}"
 
 pdf-finalize: pdf
 	rm -rf $(OUTDIRPDFFINAL) && mkdir $(OUTDIRPDFFINAL)
@@ -145,4 +170,6 @@ CLEAN_FILES += \
 	$(OUTDIRHTMLFINAL) $(OUTDIRPDFFINAL) $(OUTDIREPUBFINAL)
 
 clean:
-	rm -rf $(CLEAN_FILES)
+	rm -rf $(CLEAN_FILES) \
+	unlink ../../UG8.4/*/images \
+	unlink ../../UG9/*/images
